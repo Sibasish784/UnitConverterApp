66ce94f7588e79af122a126baf618d99cb5d5a39
Sibasish784 <01sibasish@gmail.com>
2023-03-24 08:58:14 +0530

    tree updated

--- app/src/main/java/com/example/uniconv/MainActivity.java
+++ app/src/main/java/com/example/uniconv/MainActivity.java
@@ -3,13 +3,29 @@ package com.example.uniconv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private TextView textView;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+    }
+    public void calculate(View view){
+        String s = editText.getText().toString();
+        int kg = Integer.parseInt(s);
+        int gram = 1000 * kg;
+        textView.setText("The corresponding value in Gram is " + gram);
+
     }
 }
